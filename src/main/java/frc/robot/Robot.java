a7b3d8f122b9063dc6f464381637ec2f1bc83148
jamiha <34746242+jamiha@users.noreply.github.com>
2021-03-15 18:53:10 -0400

    switch to new mechanum drive code (before code clean up)

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,6 +6,10 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.kinematics.MecanumDriveKinematics;
+import edu.wpi.first.wpilibj.kinematics.MecanumDriveWheelSpeeds;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
@@ -24,15 +28,22 @@ public class Robot extends TimedRobot {
   private double FRVoltage;
   private double BRVoltage;
 
+  MecanumDriveKinematics m_kinematics;
+
+  CANSparkMax frontLeft;
+  CANSparkMax rearLeft;
+  CANSparkMax frontRight;
+  CANSparkMax rearRight;
+
   @Override
   public void robotInit() {
     //Giving Spark Maxs there motor positions and the intiger that is assosciated with them.
     //To change Spark Max Intigers use Rev Hardware Client found on Desktop
     //MotorType is the tpye of motor we are using with the motor controller.
-    CANSparkMax frontLeft = new CANSparkMax(4, MotorType.kBrushless);
-    CANSparkMax rearLeft = new CANSparkMax(5, MotorType.kBrushless);
-    CANSparkMax frontRight = new CANSparkMax(1, MotorType.kBrushless);
-    CANSparkMax rearRight = new CANSparkMax(2, MotorType.kBrushless);
+    frontLeft = new CANSparkMax(4, MotorType.kBrushless);
+    rearLeft = new CANSparkMax(5, MotorType.kBrushless);
+    frontRight = new CANSparkMax(1, MotorType.kBrushless);
+    rearRight = new CANSparkMax(2, MotorType.kBrushless);
 
     //These are to return the voltage values we are getting for each motor to the SmartDashboard
     FLVoltage = frontLeft.getBusVoltage();
@@ -41,14 +52,36 @@ public class Robot extends TimedRobot {
     BRVoltage = frontLeft.getBusVoltage();
 
     //Invert motor direction
-    frontRight.setInverted(true);
+    frontRight.setInverted(false);
+    frontLeft.setInverted(true);
     rearRight.setInverted(true);
+    rearLeft.setInverted(false);
 
     //giving m_robotDrive the motors it requires.
-    m_robotDrive = new MecanumDrive(frontLeft, rearLeft, frontRight, rearRight);
+    //m_robotDrive = new MecanumDrive(frontLeft, rearLeft, frontRight, rearRight);
 
     //The Xbox Controller and the input intiger that corresponds with its usb port.
     controller = new XboxController(0);
+
+    Translation2d m_frontLeftLocation = new Translation2d(0.381, 0.381);
+    Translation2d m_frontRightLocation = new Translation2d(0.381, -0.381);
+    Translation2d m_backLeftLocation = new Translation2d(-0.381, 0.381);
+    Translation2d m_backRightLocation = new Translation2d(-0.381, -0.381);
+
+    // Creating my kinematics object using the wheel locations.
+    m_kinematics = new MecanumDriveKinematics(
+      m_frontLeftLocation, m_frontRightLocation, m_backLeftLocation, m_backRightLocation
+    );
+
+  }
+
+  protected double deadband(double value) {
+    double deadband = 0.05;
+    if (Math.abs(value) <= deadband) {
+      return 0.0;
+    } else {
+      return value;
+    }
   }
 
   @Override
@@ -61,8 +94,29 @@ public class Robot extends TimedRobot {
     //The deadband Utility can be found under Utilities.java
 
     //The getDeadBand funtion will take joystick input and prevent that value from moving the robot if user input is within given range which is currently .2 and -.2
-    m_robotDrive.driveCartesian(utils.getDeadBand(controller.getY(Hand.kLeft), .2), utils.getDeadBand(controller.getX(Hand.kLeft), .2), utils.getDeadBand(controller.getX(Hand.kRight), .2), 0);
+    double speedFactor = 0.25;
+    double leftY = deadband(controller.getY(Hand.kLeft)*speedFactor);
+    double leftX = deadband(controller.getX(Hand.kLeft)*speedFactor);
+    double rightX = deadband(controller.getX(Hand.kRight)*speedFactor);
+    //m_robotDrive.driveCartesian(utils.getDeadBand(controller.getY(Hand.kLeft), .2), utils.getDeadBand(controller.getX(Hand.kLeft), .2), utils.getDeadBand(controller.getX(Hand.kRight), .2), 0);
 
+    System.out.println(leftY + "; " + leftX + "; " + rightX);
+    //m_robotDrive.driveCartesian(leftY, leftX, rightX);
+
+    ChassisSpeeds speeds = new ChassisSpeeds(leftY, leftX, rightX);    
+    MecanumDriveWheelSpeeds wheelSpeeds = m_kinematics.toWheelSpeeds(speeds);
+
+    double spFrontLeft = wheelSpeeds.frontLeftMetersPerSecond;
+    double spFrontRight = wheelSpeeds.frontRightMetersPerSecond;
+    double spBackLeft = wheelSpeeds.rearLeftMetersPerSecond;
+    double spBackRight = wheelSpeeds.rearRightMetersPerSecond;
+
+    frontLeft.set(spFrontLeft);
+    frontRight.set(spFrontRight);
+    rearLeft.set(spBackLeft);
+    rearRight.set(spBackRight);
+    
+    //e480tyfg
     //This code was working originally but needed to be altared for a deadband fix for the controllers
     //m_robotDrive.driveCartesian(controller.getY(Hand.kLeft), controller.getX(Hand.kLeft), controller.getX(Hand.kRight), 0);
 
